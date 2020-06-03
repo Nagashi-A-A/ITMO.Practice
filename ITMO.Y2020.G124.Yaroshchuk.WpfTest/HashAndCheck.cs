3ab0b0c84e75dfe67efa7dff278bf1597437b1fe
Антон Ярощук <anton.yaroshchuk@yandex.ru>
2020-06-03 13:15:33 +0300

    Changes: 02.06.2020

---
+++ ITMO.Y2020.G124.Yaroshchuk.WpfTest/HashAndCheck.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.Y2020.G124.Yaroshchuk.WpfTest
+{
+    public class HashAndCheck
+    {
+        const int alpha = 31;
+        int cycleCount = 1;
+        public string finalMessage;
+        List<long> pewpews = new List<long>(10000);
+        SortedDictionary<int, long> hashes = new SortedDictionary<int, long>();
+
+        public long HashHash(string needHash)
+        {
+            long whatLen = needHash.Length;
+            long hash = 0;
+            for (int j = 0; j < whatLen; ++j)
+            {
+                hash += ((long)needHash[j] - (long)'a' + 1) * (long)pewpews[j];
+            }
+            return hash;
+        }
+
+        public HashAndCheck(List<string> stringsToCheck)
+        {
+            pewpews.Add(1);
+
+            for (int i = 1; i < 1000; i++)
+            {
+                long impera = 0;
+                impera = (pewpews[i - 1] * alpha);
+                pewpews.Add(impera);
+            }
+
+            foreach (string strings in stringsToCheck)
+            {
+                long realHash = 0;
+                realHash = HashHash(strings);
+                hashes.Add(cycleCount, realHash);
+                cycleCount++;
+            }
+
+            var stringEq = hashes.ToLookup(x => x.Value, x => x.Key).Where(x => x.Count() > 1);
+            //foreach (var item in stringEq)
+            Parallel.ForEach(stringEq, item =>
+            {
+                var keys = item.Aggregate("", (s, v) => s + " " + v);
+                var message = keys;
+                finalMessage += "\r" + "These strings are equal: " + message.ToString();
+            });
+            hashes.Clear();
+        }
+    }
+}
---
+++ ITMO.Y2020.G124.Yaroshchuk.WpfTest/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ITMO.Y2020.G124.Yaroshchuk.WpfTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int count = 1;
+        List<string> stringsToCheck = new List<string>();
+        List<string> stringsCount = new List<string>();
+        StringBuilder nStnBuild = new StringBuilder();
+        public MainWindow()
+        {
+            InitializeComponent();
+            AddStringBox.Clear();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            stringsToCheck.Add(AddStringBox.Text);
+            stringsCount.Add("№" + count.ToString());
+                nStnBuild.AppendFormat("{0}  {1}", stringsCount[count-1], stringsToCheck[count-1]);
+                nStnBuild.AppendLine();
+            StringsCheckBox.AppendText(nStnBuild.ToString());
+            nStnBuild.Clear();
+            count++;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            StringsCheckBox.Document.Blocks.Clear();
+            AddStringBox.Clear();
+            stringsToCheck.Clear();
+            stringsCount.Clear();
+            count = 1;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            HashAndCheck msgBox = new HashAndCheck(stringsToCheck);
+            if (msgBox.finalMessage != null)
+                MessageBox.Show(msgBox.finalMessage);
+            else
+                MessageBox.Show("There aren't any equal strings.");
+        }
+    }
+}
